e76b1607563f245923f7dccb507c16751f37912f
sjsfera <sjsfera@gmail.com>
2020-07-16 00:46:11 +0300

    hw1
    scripts

---
+++ hw1/task1.js
@@ -0,0 +1,79 @@
+<div>Пример:</div>
+<script class="1">
+    let a = 15.678;
+
+    let b = 123.965;
+
+    let c = 90.2345;
+
+    let max = Math.max(a, b, c);
+
+    document.writeln (max);
+</script>
+<div>Пример:</div>
+<script class="2">
+    let a = 15.678;
+
+    let b = 123.965;
+
+    let c = 90.2345;
+
+    let min = Math.min(a, b, c);
+
+    document.writeln (min);
+</script>
+<div>Пример:</div>
+<script class="3">
+    let a = 15.678;
+
+    let b = 123.965;
+    
+    let c = 90.2345;
+    
+    let d = a + b + c;
+    document.writeln(d);
+</script> 
+<div>Пример:</div>
+<script class="4">
+    let a = 15.678;
+
+    let b = 123.965;
+
+    let c = 90.2345;
+
+    let x = Math.floor(a);
+
+    let y = Math.floor(b);
+
+    let z = Math.floor(c);
+
+    let totalPrice = (x + y + z);
+
+    document.writeln(totalPrice);
+</script>
+<div>Пример:</div>
+<script class="5">
+    let a = 15.678;
+
+    let b = 123.965;
+
+    let c = 90.2345;
+
+    let price = ((a + b + c) / 100);
+
+    let x = ((Math.ceil(price)) * 100);
+
+    document.writeln(x);
+</script>
+<div>Пример:</div>
+<script class="7">
+    let a = 15.678;
+
+    let b = 123.965;
+
+    let c = 90.2345;
+
+    let price = (a + b + c);
+
+    document.writeln (500 - price);
+</script>
---
+++ hw1/task2.js
@@ -0,0 +1,11 @@
+
+            let a = 15.678;
+
+            let b = 123.965;
+
+            let c = 90.2345;
+
+            let min = Math.min(a, b, c);
+
+            document.writeln (min);
+       
